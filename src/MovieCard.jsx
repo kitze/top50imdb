739483b5057d826da20e8faa4470b8c0a3137589
Michel EDIGHOFFER <edimitchel@gmail.com>
2023-11-27 13:56:27 +0000

    feat: accessibility

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -3,11 +3,21 @@
 import React from 'react';
 
 const MovieCard = ({ title, year, image, watched, onToggleWatched }) => {
+  const ariaLabel = (() => {
+    const watchedIndicator = watched ? 'watched' : 'not watched';
+
+    // If not decided yet, don't add information about watch state
+    if (watched === undefined) return '';
+
+    return `${title} ${watched !== null ? `is ${watchedIndicator}` : ''}`;
+  })(watched)
+
   return (
     <button
       role="button"
       className={`movie-card ${watched ? 'watched' : ''}`}
       onClick={onToggleWatched}
+      aria-label={ariaLabel}
     >
       <img src={image} alt={title} />
       <div className="movie-details">
